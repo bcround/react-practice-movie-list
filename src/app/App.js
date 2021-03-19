7bfd567adcd893391f4f48684f18613adad3c0cf
bcround <sbc1142@gmail.com>
2021-03-19 22:48:34 +0900

    feat: Complete a draft copy of movie list app component

--- src/app/App.js
+++ src/app/App.js
@@ -1,23 +1,40 @@
-import './App.scss'
-import logo from 'assets/logo.svg'
+import {useState, useEffect} from 'react'
+
 
 function App() {
+  const [movies, setMovies] = useState([])
+  const [hasError, setHasError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    fetch("https://yts.mx/api/v2/list_movies.json")
+      .then(response => response.json())
+      .then(({ data }) => {
+        console.log(data)
+        setMovies(data.movies)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>src/App.js</code> 문서를 수정하면 실시간 업데이트 됩니다
-        </p>
-        <a
-          className="App-link"
-          href="https://ko.reactjs.org"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          React를 배워보세요
-        </a>
-      </header>
+      <h1 className="movieHeading">List of Movies</h1>
+      <div className="movieBody">
+        <ul className="movieList">
+          {
+            movies.map(movie => {
+              return(
+                <li key={movie.id}>
+                  <figure>
+                    <img src={movie.background_image} alt={movie.title}/>
+                    <figcaption>{movie.title_long}</figcaption>
+                  </figure>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
     </div>
   )
 }
