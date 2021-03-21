77178b2f49ac44c5108028959b467f487a6086d2
bcround <sbc1142@gmail.com>
2021-03-21 17:14:47 +0900

    feat: Finish the styling and add search box

--- src/app/App.js
+++ src/app/App.js
@@ -1,40 +1,69 @@
 import {useState, useEffect} from 'react'
-
+import Movie from '../components/Movie/Movie'
+import { ReactComponent as Rainbow} from '../assets/rainbow.svg'
 
 function App() {
   const [movies, setMovies] = useState([])
+  const [movieName, setMovieName] = useState("")
   const [hasError, setHasError] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
+  const loadingStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  }
+
+  const onSearch = e => {
+    e.preventDefault()
+    setMovieName(e.target.previousElementSibling.value)
+  }
 
   useEffect(() => {
+    setIsLoading(true)
+
     fetch("https://yts.mx/api/v2/list_movies.json")
       .then(response => response.json())
       .then(({ data }) => {
-        console.log(data)
         setMovies(data.movies)
+        setIsLoading(false)
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        setHasError(err)
+        setIsLoading(false)
+      })
   }, [])
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    fetch(`https://yts.mx/api/v2/list_movies.json?query_term=${movieName}`)
+      .then(response => response.json())
+      .then(({data}) => {
+        if (data.movie_count !== 0) setMovies(data.movies)
+        else setMovies(['No movies found'])
+        setIsLoading(false)
+      })
+      .catch(err => {
+        setHasError(err)
+        setIsLoading(false)
+      })
+  }, [movieName])
+
+  if (isLoading) {
+    return <Rainbow style={loadingStyle}/>
+  }
+
+  if (hasError) {
+    return <div role="alert">{hasError.message}</div>
+  }
 
   return (
     <div className="App">
-      <h1 className="movieHeading">List of Movies</h1>
-      <div className="movieBody">
-        <ul className="movieList">
-          {
-            movies.map(movie => {
-              return(
-                <li key={movie.id}>
-                  <figure>
-                    <img src={movie.background_image} alt={movie.title}/>
-                    <figcaption>{movie.title_long}</figcaption>
-                  </figure>
-                </li>
-              )
-            })
-          }
-        </ul>
-      </div>
+      <Movie>
+        <Movie.SearchBox onSearch={onSearch} labelContent="" buttonContent="검색"/>
+        <Movie.List movies={movies} />
+      </Movie>
     </div>
   )
 }
